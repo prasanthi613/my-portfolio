1c324ed71e05caeb18f64831831bf9480b0c33aa
prasanthi613 <87223788+prasanthi613@users.noreply.github.com>
2025-06-16 19:06:56 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,18 @@ export default function Hero() {
     <section id="home" className="bg-dark text-white py-5 mt-5">
       <Container className="text-center">
         <h1 className="display-4 mb-3">Hi, I'm Lakshmi 👋</h1>
-       <p className="typing-text lead text-light fst-italic">
-   I build elegant, responsive, and scalable web applications.
+       <p
+  className="lead fw-semibold fst-italic text-center px-3"
+  style={{
+    fontSize: '1.1rem',
+    lineHeight: '1.6',
+    maxWidth: '90%',
+    margin: '0 auto',
+  }}
+>
+  I build elegant, responsive, and scalable web applications.
 </p>
+
 
         <img
           src="/profile.png"
